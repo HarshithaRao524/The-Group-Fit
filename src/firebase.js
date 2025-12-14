e5b863ac0c77c5ccb49af5ff9d4cca43e47c47a9
Harshitha Rao <harshitharrao5@gmail.com>
2025-12-14 19:36:39 +0530

    Remove hardcoded Firebase keys

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 import { getFunctions } from "firebase/functions";   // ✅ Import functions
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD3VhidnYaBDV_E6WICl0PYEgO-WzkotUk",
-  authDomain: "the-group-fit-firebase.firebaseapp.com",
-  projectId: "the-group-fit-firebase",
-  storageBucket: "the-group-fit-firebase.firebasestorage.app",
-  messagingSenderId: "258362312319",
-  appId: "1:258362312319:web:0f7f1095d938d1e20ce14a",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
   databaseURL: "https://the-group-fit-firebase-default-rtdb.asia-southeast1.firebasedatabase.app/"
 };
 
