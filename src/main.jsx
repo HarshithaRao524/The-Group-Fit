d3556b74ae4487c68a235e757ba605ec36bdbb47
Harshitha Rao <harshitharrao5@gmail.com>
2026-01-21 22:51:29 +0530

    Fix routing by adding BrowserRouter with AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/globals.css";
-import { AuthProvider } from "./contexts/AuthContext"; // ✅ Import AuthProvider
+import { AuthProvider } from "./contexts/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* ✅ Wrap App inside AuthProvider */}
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
